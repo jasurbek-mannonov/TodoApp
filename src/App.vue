9dfb41621b8c2bb7189f6605190d3985d01ed318
jasurbek-mannonov <jasurbekmannonov2020@gmail.com>
2023-10-03 21:41:19 +0500

    Updated

--- src/App.vue
+++ src/App.vue
@@ -51,21 +51,20 @@ onMounted(() => {
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        What's up, <input type="text" placeholder="Name here" v-model="name">
+        Assalomu alaykum, <input type="text" placeholder="Ismingizni kiriting" v-model="name">
       </h2>
     </section>
 
     <section class="create-todo">
-      <h3>CREATE A TODO</h3>
+      <h3>Vazifa qo'shish</h3>
 
       <form @submit.prevent="addTodo()">
-        <h4>What's on your todo list?</h4>
         <input 
         type="text" 
-        placeholder="e.g.make a video" 
+        placeholder="Misol uchun: bozorga borish" 
         v-model="input_content" /> 
 
-        <h4>Pick a category</h4>
+        <h4>Kategoriyani tanlang</h4>
 
         <div class="options">
           <label>
@@ -76,7 +75,7 @@ onMounted(() => {
             v-model="input_category"
             />
             <span class="bubble business"></span>
-            <div>Business</div>
+            <div>Ish</div>
           </label>
 
           <label>
@@ -87,17 +86,17 @@ onMounted(() => {
             v-model="input_category"
             />
             <span class="bubble personal"></span>
-            <div>Personal</div>
+            <div>Shaxsiy</div>
           </label>
 
           
         </div>
-        <input type="submit" value="Add todo"/>
+        <input type="submit" value="Qo'shish"/>
       </form>
     </section>
 
     <section class="todo-list">
-      <h3>TODO LIST</h3>
+      <h3>Ro'yxat</h3>
       <div class="list">
         <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
           <label>
@@ -110,7 +109,7 @@ onMounted(() => {
           </div>
 
           <div class="actions">
-            <button class="delete" @click="removeTodo(todo)">Delete</button>
+            <button class="delete" @click="removeTodo(todo)">O'chirish</button>
           </div>
         </div>
       </div>
